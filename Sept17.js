8ccbd61367698154b5f05daa9cad4c6206fdae02
MiaRobangK25 <b2029asrobang@pshs.edu.ph>
2025-09-17 12:50:27 +0800

    pusheth

--- Sept17.js
+++ Sept17.js
@@ -9,7 +9,6 @@ for (let i = 0; i < n; i++)
     }
 }
   console.log(row);
-
 // solid 5x5 square
 
 for (let i = 1; i <= n; i++) 
@@ -21,7 +20,6 @@ for (let i = 1; i <= n; i++)
     }
     console.log(row);
 }
-
 // right triangle
 
 for (let i = 1; i <= n; i++) 
@@ -37,7 +35,6 @@ for (let i = 1; i <= n; i++)
     }
     console.log(row);
 }
- 
 // reflected right triangle
 
 for (let i = 1; i <= n; i++) 
@@ -54,3 +51,28 @@ for (let i = 1; i <= n; i++)
     console.log(row);
   }
 // pyramid  
+
+let size = 6; 
+for (let i= 0; i < size; i++)
+{
+  let row = ""
+  for (let j = 0; j < size; j++)
+  {
+    row += (i+j) % 2 === 0 ? '#' : "@";
+  }
+  console.log(row);
+}
+// chessboard 6x6 
+
+let a = 0; 
+let b = 1; 
+let result = a + " " + b + " ";
+for (let i = 3; i <= 10; i++)
+{
+  let next = a + b; 
+  result += next + ' ';
+  a = b; 
+  b = next; 
+}
+console.log(result.trim())
+// fibonnaci sequence 
